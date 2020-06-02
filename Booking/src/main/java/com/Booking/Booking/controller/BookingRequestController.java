daad6a15be50e14900c725712122de98a2b60727
NemanjaKajzer <kajzapfk@gmail.com>
2020-06-02 22:41:54 +0200

    Filter radi delimicno

--- Booking/src/main/java/com/Booking/Booking/controller/BookingRequestController.java
+++ Booking/src/main/java/com/Booking/Booking/controller/BookingRequestController.java
@@ -1,5 +1,6 @@
 package com.Booking.Booking.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import com.Booking.Booking.dtos.ItemInCartDTO;
 import com.Booking.Booking.enums.RequestStates;
 import com.Booking.Booking.model.ItemInCart;
 import com.Booking.Booking.model.requests.BookingRequest;
+import com.Booking.Booking.repository.BookingRequestRepository;
 import com.Booking.Booking.service.BookingRequestService;
 import com.Booking.Booking.service.ItemInCartService;
 import com.Booking.Booking.service.ShoppingCartService;
@@ -22,28 +24,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping(value = "booking")
+// @RequestMapping(value = "booking")
 public class BookingRequestController {
 
-@Autowired
-BookingRequestService bookingRequestService;
+    @Autowired
+    BookingRequestService bookingRequestService;
 
-@Autowired
-ItemInCartService itemInCartService;
+    @Autowired
+    ItemInCartService itemInCartService;
 
-@Autowired
-ShoppingCartService shoppingCartService;
+    @Autowired
+    ShoppingCartService shoppingCartService;
 
+    @GetMapping("/hello")
+    public ResponseEntity<?> get() {
 
-@GetMapping("/hello")
-public ResponseEntity<?> get() {
-   
-    
-    return new ResponseEntity<>(String.format("OMMMMMMGMF"), HttpStatus.OK);
-}
+        return new ResponseEntity<>(String.format("OMMMMMMGMF"), HttpStatus.OK);
+    }
 
-    @PostMapping(value = "/save", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<ItemInCart>> Login(@RequestBody List<ItemInCartDTO> lista){
+    @PostMapping(value = "/save", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ItemInCart>> Login(@RequestBody List<ItemInCartDTO> lista) {
 
         System.out.println("Pogodio je back");
 
@@ -51,39 +51,31 @@ public ResponseEntity<?> get() {
 
         itemInCartService.removeAll();
 
-
-
-
-        return new ResponseEntity<>(shoppingCartService.fotCart(),HttpStatus.OK);
+        return new ResponseEntity<>(shoppingCartService.fotCart(), HttpStatus.OK);
     }
 
     @PostMapping(value = "/getAllForAgent")
-    public ResponseEntity<List<BookingRequest>> getAllSpecificForAgent(@RequestBody RequestStates state){
+    public ResponseEntity<List<BookingRequest>> getAllSpecificForAgent(@RequestBody RequestStates state) {
 
-		
-	   
-		List<BookingRequest> requests = bookingRequestService.getAllSpecificForAgent(state);
+        List<BookingRequest> requests = bookingRequestService.getAllSpecificForAgent(state);
 
-		System.out.println("pogodio je kontroler, broj oglasa vraca=="+requests.size());
-		
+        System.out.println("pogodio je kontroler, broj oglasa vraca==" + requests.size());
+
         return new ResponseEntity<>(requests, HttpStatus.OK);
     }
 
-  
-    
     @PostMapping(value = "/getGroupsForAgent")
-    public ResponseEntity<List<Long>> getAllSpecificGroupsForAgent(@RequestBody RequestStates state){
-                
-		List<Long> groups = bookingRequestService.getSpecificGroupsForAgent(state);
+    public ResponseEntity<List<Long>> getAllSpecificGroupsForAgent(@RequestBody RequestStates state) {
 
-		System.out.println("pogodio je kontroler, broj grupa=="+groups.size());
-		
+        List<Long> groups = bookingRequestService.getSpecificGroupsForAgent(state);
+
+        System.out.println("pogodio je kontroler, broj grupa==" + groups.size());
+
         return new ResponseEntity<>(groups, HttpStatus.OK);
     }
 
-
     @PostMapping(value = "/acceptRequest")
-    public ResponseEntity<Long> acceptRequest(@RequestBody Long grupa){
+    public ResponseEntity<Long> acceptRequest(@RequestBody Long grupa) {
 
         System.out.println("Pogodio prihvatanje zahteva grupe: " + grupa);
 
@@ -91,38 +83,63 @@ public ResponseEntity<?> get() {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
-    
-   
 
     @PostMapping(value = "/getAllSpecificForBuyer")
-    public ResponseEntity<List<BookingRequest>> getAllSpecificForBuyer(@RequestBody RequestStates state){
+    public ResponseEntity<List<BookingRequest>> getAllSpecificForBuyer(@RequestBody RequestStates state) {
 
         List<BookingRequest> requests = bookingRequestService.getAllSpecificForBuyer(state);
 
-		System.out.println("pogodio je kontroler, broj oglasa vraca=="+requests.size());
-		
+        System.out.println("pogodio je kontroler, broj oglasa vraca==" + requests.size());
+
         return new ResponseEntity<>(requests, HttpStatus.OK);
     }
 
     @PostMapping(value = "/grups")
-    public ResponseEntity<List<Long>> getAllSpecificGroupsForBuyer(@RequestBody RequestStates state){
+    public ResponseEntity<List<Long>> getAllSpecificGroupsForBuyer(@RequestBody RequestStates state) {
 
         List<Long> groups = bookingRequestService.getSpecificGroupsForBuyer(state);
 
-		System.out.println("pogodio je kontroler, broj grupa=="+groups.size());
-		
+        System.out.println("pogodio je kontroler, broj grupa==" + groups.size());
+
         return new ResponseEntity<>(groups, HttpStatus.OK);
     }
 
     @PostMapping(value = "/cancelRequest")
-    public ResponseEntity<Long> cancelRequest(@RequestBody Long group ){
-
+    public ResponseEntity<Long> cancelRequest(@RequestBody Long group) {
 
         bookingRequestService.cancelRequest(group);
-		
-		
-        return new ResponseEntity<>( HttpStatus.OK);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    
+    // @GetMapping(value = "/getAllBookings")
+    // public ResponseEntity<List<BookingRequest>> getAllBookings() {
+    // List<BookingRequest> booked = new ArrayList<BookingRequest>();
+    // List<BookingRequest> all = bookingRequestService.findAll();
+
+    // for (BookingRequest request : all) {
+    // if ((request.getStateOfRequest().name().equals("RESERVED")
+    // || request.getStateOfRequest().name().equals("PAID"))
+    // && request.getTimeFrom().isAfter(LocalDateTime.now())) {
+    // booked.add(request);
+    // }
+    // }
+    // return new ResponseEntity<>(booked, HttpStatus.OK);
+    // }
+
+    @GetMapping(value = "/getAllBookings")
+    public ResponseEntity<List<BookingRequest>> getAllBookings() {
+        List<BookingRequest> booked = new ArrayList<BookingRequest>();
+        List<BookingRequest> all = bookingRequestService.findAll();
+
+        for (BookingRequest request : all) {
+            if ((request.getStateOfRequest().name().equals("RESERVED")
+                    || request.getStateOfRequest().name().equals("PAID"))
+                    && request.getTimeFrom().isAfter(LocalDateTime.now())) {
+                booked.add(request);
+            }
+        }
+        return new ResponseEntity<>(booked, HttpStatus.OK);
+    }
+
 }
