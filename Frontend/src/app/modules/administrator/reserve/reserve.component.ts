c1a7aa5ab4c309e8da6f879496920b10b322ee24
abuhmiler <abuhmiler4@gmail.com>
2020-06-03 01:07:06 +0200

    ispravka

--- Frontend/src/app/modules/administrator/reserve/reserve.component.ts
+++ Frontend/src/app/modules/administrator/reserve/reserve.component.ts
@@ -15,26 +15,34 @@ export class ReserveComponent implements OnInit {
   timeFrom: Date;
   timeTo: Date;
   reserveForm: FormGroup;
+  adName: string;
 
-  constructor(private advertisementService: AdvertisementService, private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-
-    this.reserveForm = this.formBuilder.group({
-      timeFrom: [''],
-      timeTo: ['']
-    });
-
+  constructor(private advertisementService: AdvertisementService, private formBuilder: FormBuilder) {
     this.advertisementService.getAll().subscribe(
       data => {
         this.advertisements = data;
       });
+   }
+
+  ngOnInit() {
+
+    this.reserveForm = this.formBuilder.group({
+      advSelect: [''],
+      timeFrom: [''],
+      timeTo: ['']
+    });
+
   }
 
   onSubmit() {
+    for(let i = 0; i < this.advertisements.length; i++) {
+      if(this.advertisements[i].name === this.adName) {
+        this.adv = this.advertisements[i];
+      }
+    }
     this.adv.reservedFrom = this.timeFrom;
     this.adv.reservedTo = this.timeTo;
-    this.advertisementService.update(this.adv);
+    this.advertisementService.update(this.adv).subscribe();
   }
 
 }
