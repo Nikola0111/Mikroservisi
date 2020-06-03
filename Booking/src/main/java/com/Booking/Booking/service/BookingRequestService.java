66568c4fa46f77e560112a209fd996e094ec22d0
NemanjaKajzer <kajzapfk@gmail.com>
2020-06-03 22:50:11 +0200

    Pri rucnom rezervisanju ponistava sve bookinge koji su u bilo kakvom preseku sa trazenim terminom

--- Booking/src/main/java/com/Booking/Booking/service/BookingRequestService.java
+++ Booking/src/main/java/com/Booking/Booking/service/BookingRequestService.java
@@ -31,224 +31,184 @@ public class BookingRequestService {
     @Autowired
     RestTemplate restTemplate;
 
-   
-//PUCACE JER LogedUserId MORA DA SE DOBAVI NEKAKO
+    // PUCACE JER LogedUserId MORA DA SE DOBAVI NEKAKO
 
-
-    public void makeRequests(List<ItemInCartDTO> listaZahteva){
-        //pretrazujem za svakog vlasnika, ako se pogodi da je isti, pogledam dal ide oglas odvojeno ili zajedno i na osnovu toga 
-        //ubacujem u listu, cuvam i obrisem, onda se predje na sledeceg vlasnika
-        //EROR STA AKO JE U PRVOJ LISTI OPET ISTI
-        List<Long> vlasnici=new ArrayList<Long>();
-        List<ItemInCartDTO> copyList=listaZahteva;
-        ArrayList<ItemInCartDTO> together=new ArrayList<>();
-        ArrayList<ItemInCartDTO> seprate=new ArrayList<>();
-
+    public void makeRequests(List<ItemInCartDTO> listaZahteva) {
+        // pretrazujem za svakog vlasnika, ako se pogodi da je isti, pogledam dal ide
+        // oglas odvojeno ili zajedno i na osnovu toga
+        // ubacujem u listu, cuvam i obrisem, onda se predje na sledeceg vlasnika
+        // EROR STA AKO JE U PRVOJ LISTI OPET ISTI
+        List<Long> vlasnici = new ArrayList<Long>();
+        List<ItemInCartDTO> copyList = listaZahteva;
+        ArrayList<ItemInCartDTO> together = new ArrayList<>();
+        ArrayList<ItemInCartDTO> seprate = new ArrayList<>();
 
         for (ItemInCartDTO ItemInCartDTO : listaZahteva) {
-            
-            if(!vlasnici.contains(ItemInCartDTO.getAdvertisementPostedById())){
+
+            if (!vlasnici.contains(ItemInCartDTO.getAdvertisementPostedById())) {
 
                 vlasnici.add(ItemInCartDTO.getAdvertisementPostedById());
 
             }
         }
 
-        System.out.println("Broj vlasnika je : "+vlasnici.size());
+        System.out.println("Broj vlasnika je : " + vlasnici.size());
 
+        for (Long id : vlasnici) {
 
-        for (Long id: vlasnici) {
-            
-            for (ItemInCartDTO adv :copyList) {
-                
-                if(adv.getAdvertisementPostedById().equals(id)){
+            for (ItemInCartDTO adv : copyList) {
 
-                    if(adv.isTogether()){
+                if (adv.getAdvertisementPostedById().equals(id)) {
 
+                    if (adv.isTogether()) {
 
                         together.add(adv);
 
-                    }
-                    else{
+                    } else {
                         seprate.add(adv);
 
                     }
 
                 }
 
-
-
             }
 
             // treba ovde da sejvujem ako nisu prazne i da ih ispraznim nakon sejvanja
-           BookingRequest request=bookingRequestRepository.findTopByOrderByIdDesc();
-           Long lastGroupId;
-         
-           System.out.println(request);
-            
-            if(request==null){
-                lastGroupId=Long.valueOf(0);
+            BookingRequest request = bookingRequestRepository.findTopByOrderByIdDesc();
+            Long lastGroupId;
 
-            }
-            else{
-                lastGroupId=request.getGroupId();
+            System.out.println(request);
+
+            if (request == null) {
+                lastGroupId = Long.valueOf(0);
+
+            } else {
+                lastGroupId = request.getGroupId();
             }
 
             // try{
-            //     lastGroupId=request.getGroupId();
-               
+            // lastGroupId=request.getGroupId();
 
             // }
             // catch(Exception e){
-            //     lastGroupId=Long.valueOf(0);
-              
+            // lastGroupId=Long.valueOf(0);
+
             // }
 
-           
+            System.out.println("Poslednji id je : " + lastGroupId);
 
-            System.out.println("Poslednji id je : "+lastGroupId);
-            
-            if(!seprate.isEmpty()){
+            if (!seprate.isEmpty()) {
 
                 for (ItemInCartDTO ItemInCartDTO2 : seprate) {
-                
-                lastGroupId++;
-                bookingRequestRepository.save(new BookingRequest( ItemInCartDTO2.getAdvertisementPostedById(),getLogedUserId(),
-                 lastGroupId, RequestStates.PENDING,ItemInCartDTO2.getAdvertisementId() , ItemInCartDTO2.isTogether(),ItemInCartDTO2.getTimeFrom(),ItemInCartDTO2.getTimeTo()));
 
+                    lastGroupId++;
+                    bookingRequestRepository.save(new BookingRequest(ItemInCartDTO2.getAdvertisementPostedById(),
+                            getLogedUserId(), lastGroupId, RequestStates.PENDING, ItemInCartDTO2.getAdvertisementId(),
+                            ItemInCartDTO2.isTogether(), ItemInCartDTO2.getTimeFrom(), ItemInCartDTO2.getTimeTo()));
 
-
-                 
                 }
                 seprate.clear();
 
-               
-
             }
 
+            lastGroupId++;
 
-           lastGroupId++;
-
-            if(!together.isEmpty()){
+            if (!together.isEmpty()) {
 
                 for (ItemInCartDTO ItemInCartDTO : together) {
 
-                    //GET LoggedUserId MORA DA SE DOBAVI NEKAKO ZATO PUCAA
-                    
-                    bookingRequestRepository.save(new BookingRequest( ItemInCartDTO.getAdvertisementPostedById(),getLogedUserId(),
-                    lastGroupId, RequestStates.PENDING,ItemInCartDTO.getAdvertisementId(),
-                     ItemInCartDTO.isTogether(),ItemInCartDTO.getTimeFrom(),ItemInCartDTO.getTimeTo()));
+                    // GET LoggedUserId MORA DA SE DOBAVI NEKAKO ZATO PUCAA
 
+                    bookingRequestRepository.save(new BookingRequest(ItemInCartDTO.getAdvertisementPostedById(),
+                            getLogedUserId(), lastGroupId, RequestStates.PENDING, ItemInCartDTO.getAdvertisementId(),
+                            ItemInCartDTO.isTogether(), ItemInCartDTO.getTimeFrom(), ItemInCartDTO.getTimeTo()));
 
                 }
 
                 together.clear();
             }
 
-
-           
-
         }
 
-        
-        
     }
 
+    public List<BookingRequest> getAllForRenter() {
 
-    public List<BookingRequest> getAllForRenter(){
-       
         return bookingRequestRepository.findByUserForId(getLogedUserId());
     }
 
-    public void cancelRequest(Long group){
+    public void cancelRequest(Long group) {
 
         for (BookingRequest request : bookingRequestRepository.findAllByGroupId(group)) {
-            
+
             request.setStateOfRequest(RequestStates.CANCELED);
             bookingRequestRepository.save(request);
         }
 
     }
 
-   
+    public List<BookingRequest> getAllSpecificForAgent(RequestStates state) {
+        List<BookingRequest> needed = new ArrayList<BookingRequest>();
 
-   
-
-  
-
-    public List<BookingRequest> getAllSpecificForAgent(RequestStates state){
-        List<BookingRequest> needed=new ArrayList<BookingRequest>();
- 
- 
         for (BookingRequest bookingRequest : bookingRequestRepository.findByUserForId(getLogedUserId())) {
-            
-             
- 
- 
-             if(bookingRequest.getStateOfRequest().equals(state)){
-                 needed.add(bookingRequest);
-             }
- 
+
+            if (bookingRequest.getStateOfRequest().equals(state)) {
+                needed.add(bookingRequest);
+            }
+
         }
-         return needed;
-     }
+        return needed;
+    }
 
-
-     public List<Long> getSpecificGroupsForAgent (RequestStates state){
-        List<Long> group=new ArrayList<Long>();
+    public List<Long> getSpecificGroupsForAgent(RequestStates state) {
+        List<Long> group = new ArrayList<Long>();
         System.out.println(getLogedUserId());
-        
-        for (BookingRequest bookingRequest : bookingRequestRepository.findByUserForId(getLogedUserId())) {
-            
-           
 
-            if(!group.contains(bookingRequest.getGroupId())){
-               
-                if(bookingRequest.getStateOfRequest().equals(state)){
-                   
-                group.add(bookingRequest.getGroupId());
+        for (BookingRequest bookingRequest : bookingRequestRepository.findByUserForId(getLogedUserId())) {
+
+            if (!group.contains(bookingRequest.getGroupId())) {
+
+                if (bookingRequest.getStateOfRequest().equals(state)) {
+
+                    group.add(bookingRequest.getGroupId());
                 }
             }
         }
 
         return group;
 
-
     }
 
-    public List<BookingRequestFrontDTO> getAllSpecificForBuyer(RequestStates state){
-        List<BookingRequest> needed=new ArrayList<BookingRequest>();
-        List<BookingRequestFrontDTO> forFront=new ArrayList<BookingRequestFrontDTO>();
- 
-        List<AdvertisementCreationDTO> advertisements = restTemplate.exchange("http://advert/getAllAdvertisementsForCart", HttpMethod.GET, null,
-        new ParameterizedTypeReference<List<AdvertisementCreationDTO>>() {
-        }).getBody();
+    public List<BookingRequestFrontDTO> getAllSpecificForBuyer(RequestStates state) {
+        List<BookingRequest> needed = new ArrayList<BookingRequest>();
+        List<BookingRequestFrontDTO> forFront = new ArrayList<BookingRequestFrontDTO>();
 
-
+        List<AdvertisementCreationDTO> advertisements = restTemplate
+                .exchange("http://advert/getAllAdvertisementsForCart", HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<AdvertisementCreationDTO>>() {
+                        })
+                .getBody();
 
         for (BookingRequest bookingRequest : bookingRequestRepository.findByUserToId(getLogedUserId())) {
-            
-             System.out.println("STATE="+state);
-             System.out.println("bookingState="+bookingRequest.getStateOfRequest());
- 
- 
-             if(bookingRequest.getStateOfRequest().equals(state)){
-                 needed.add(bookingRequest);
-             }
 
-            
- 
+            System.out.println("STATE=" + state);
+            System.out.println("bookingState=" + bookingRequest.getStateOfRequest());
+
+            if (bookingRequest.getStateOfRequest().equals(state)) {
+                needed.add(bookingRequest);
+            }
+
         }
 
         for (AdvertisementCreationDTO advertisementCreationDTO : advertisements) {
-            
-            for (BookingRequest book : needed) {
-                
-                if(book.getAdvertisementId().equals(advertisementCreationDTO.getId())){
 
-                    BookingRequestFrontDTO novi=new BookingRequestFrontDTO(book.getId(),book.getUserForId(),book.getUserToId(),
-                    book.getGroupId(),book.getStateOfRequest(),advertisementCreationDTO,book.isTogether(),book.getTimeFrom(),
-                    book.getTimeTo());
+            for (BookingRequest book : needed) {
+
+                if (book.getAdvertisementId().equals(advertisementCreationDTO.getId())) {
+
+                    BookingRequestFrontDTO novi = new BookingRequestFrontDTO(book.getId(), book.getUserForId(),
+                            book.getUserToId(), book.getGroupId(), book.getStateOfRequest(), advertisementCreationDTO,
+                            book.isTogether(), book.getTimeFrom(), book.getTimeTo());
 
                     forFront.add(novi);
                 }
@@ -257,46 +217,40 @@ public class BookingRequestService {
 
         }
 
+        return forFront;
+    }
 
-         return forFront;
-     }
- 
-
-
-    public List<Long> getSpecificGroupsForBuyer (RequestStates state){
-        List<Long> group=new ArrayList<Long>();
+    public List<Long> getSpecificGroupsForBuyer(RequestStates state) {
+        List<Long> group = new ArrayList<Long>();
         System.out.println(getLogedUserId());
-        
+
         for (BookingRequest bookingRequest : bookingRequestRepository.findByUserToId(getLogedUserId())) {
-            
+
             System.out.println("prolazi kroz for");
 
-            if(!group.contains(bookingRequest.getGroupId())){
+            if (!group.contains(bookingRequest.getGroupId())) {
                 System.out.println("Usao u if");
-                if(bookingRequest.getStateOfRequest().equals(state)){
+                if (bookingRequest.getStateOfRequest().equals(state)) {
                     System.out.println("Usao drugi if");
-                group.add(bookingRequest.getGroupId());
+                    group.add(bookingRequest.getGroupId());
                 }
             }
         }
 
         return group;
 
-
     }
 
-    public List<BookingRequest> findAll()
-    {
+    public List<BookingRequest> findAll() {
         return bookingRequestRepository.findAll();
     }
 
-    public void acceptRequest(Long grupa){
+    public void acceptRequest(Long grupa) {
 
-        System.out.println("Broj zahteva"+bookingRequestRepository.findAllByGroupId(grupa).size());
+        System.out.println("Broj zahteva" + bookingRequestRepository.findAllByGroupId(grupa).size());
 
         for (BookingRequest request : bookingRequestRepository.findAllByGroupId(grupa)) {
-            
-            
+
             request.setStateOfRequest(RequestStates.RESERVED);
 
             bookingRequestRepository.save(request);
@@ -304,45 +258,52 @@ public class BookingRequestService {
         }
     }
 
-    public Long getLogedUserId(){
-    
+    public Long getLogedUserId() {
 
-        Long id = restTemplate.exchange("http://auth/getUserId", HttpMethod.GET, null, 
-        new ParameterizedTypeReference<Long>() {} ).getBody();
-    
-        System.out.println("Nasao je id="+id);
-    
+        Long id = restTemplate
+                .exchange("http://auth/getUserId", HttpMethod.GET, null, new ParameterizedTypeReference<Long>() {
+                }).getBody();
+
+        System.out.println("Nasao je id=" + id);
+
         return id;
     }
 
-    public void saveReserve(ReservationDTO reservationDTO){
-        BookingRequest toBook = new BookingRequest(getLogedUserId(),reservationDTO.getAdvertisementId(), reservationDTO.getTimeFrom(), reservationDTO.getTimeTo(),RequestStates.PAID);
+    public void saveReserve(ReservationDTO reservationDTO) {
+        BookingRequest toBook = new BookingRequest(getLogedUserId(), reservationDTO.getAdvertisementId(),
+                reservationDTO.getTimeFrom(), reservationDTO.getTimeTo(), RequestStates.PAID);
 
+        List<BookingRequest> bookedTimes = bookingRequestRepository.findAll();
 
-
-        List<BookingRequest>bookedTimes=bookingRequestRepository.findAll();
-        
         LocalDateTime timeFrom = toBook.getTimeFrom();
         LocalDateTime timeTo = toBook.getTimeTo();
-        
+
         for (BookingRequest booking : bookedTimes) {
             if (booking.getAdvertisementId() == toBook.getAdvertisementId()) {
-        
+
                 if (timeFrom.isAfter(booking.getTimeFrom()) && timeFrom.isBefore(booking.getTimeTo())) {
                     booking.setStateOfRequest(RequestStates.CANCELED);
                     bookingRequestRepository.save(booking);
-
                 }
-        
+
                 if (timeTo.isAfter(booking.getTimeFrom()) && timeTo.isBefore(booking.getTimeTo())) {
+                    booking.setStateOfRequest(RequestStates.CANCELED);
+                    bookingRequestRepository.save(booking);
+                }
+
+                if (booking.getTimeFrom().isAfter(timeFrom) && booking.getTimeFrom().isBefore(timeTo)) {
+                    booking.setStateOfRequest(RequestStates.CANCELED);
+                    bookingRequestRepository.save(booking);
+                }
+
+                if (booking.getTimeTo().isAfter(timeFrom) && booking.getTimeTo().isBefore(timeTo)) {
                     booking.setStateOfRequest(RequestStates.CANCELED);
                     bookingRequestRepository.save(booking);
                 }
             }
         }
-        
-        bookingRequestRepository.save(toBook);
 
+        bookingRequestRepository.save(toBook);
 
     }
 
